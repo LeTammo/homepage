706b133516c36862d1cf1e8080c3115c49bbb830
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2024-07-22 21:01:16 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Me' });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Projects' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
