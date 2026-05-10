43ec6d16f7c4da23d8372f60ccd04d6e98110e43
Felix Jonas Wiegleb <js-wiegleb@t-online.de>
2026-05-10 07:45:56 +0200

    Refactor nav list items

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 
 const app = express();
@@ -10,9 +9,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const globals = {
-    'navItems' : JSON.parse(fs.readFileSync('src/config/navigation.json', 'utf-8')),
-}
+const globals = {};
 
 app.get('/', (req, res) => {
     res.render('home/home', { ...globals, ...{ activePage: 'home' } });
